373543e41c4c215a7defd3f781742c731ecd83d5
Andrzej Dubaj <dubaj.andrzej@gmail.com>
2018-04-22 20:37:57 +0200

    Start from rea

--- test/api.js
+++ test/api.js
@@ -2,11 +2,9 @@ import test from 'ava'
 import signal from '../src/signal'
 
 test('has a function "change"', t => {
-  console.log(signal)
   t.is(typeof signal.change, 'function')
 })
 
 test('has a function "getCurrent"', t => {
-  console.log(signal)
   t.is(typeof signal.getCurrent, 'function')
 })
