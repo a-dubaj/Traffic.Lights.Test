c05d4cc833c2d352e775bf24a4e958b7ff3bbbed
Andrzej Dubaj <dubaj.andrzej@gmail.com>
2018-04-29 19:58:24 +0200

    Added changes: Return new signalisator instance for every test

--- src/signal.js
+++ src/signal.js
@@ -1,24 +1,26 @@
-let current = 'red'
-let gointToGreen = true
+module.exports = () => {
+  let current = 'red'
+  let gointToGreen = true
 
-module.exports = {
-  change: () => {
-    if(current === 'red') {
-      current = 'yellow'
-    } else if(current === 'yellow'){
-      if(gointToGreen){
-          current = 'green'
-          gointToGreen = false
-      } else {
-        current = 'red'
-        gointToGreen = true
-      }
-    } else if(current === 'green'){
-      current ='yellow'
+    return {
+      change: () => {
+        if(current === 'red') {
+          current = 'yellow'
+        } else if(current === 'yellow'){
+          if(gointToGreen){
+              current = 'green'
+              gointToGreen = false
+          } else {
+            current = 'red'
+            gointToGreen = true
+          }
+        } else if(current === 'green'){
+          current ='yellow'
+        }
+      },
+
+      getCurrent: () => {
+        return current
+      },
     }
-  },
-
-  getCurrent: () => {
-    return current
-  },
 }
--- test/light.js
+++ test/light.js
@@ -1,31 +1,55 @@
 import test from 'ava'
-import signal from '../src/signal'
+import getSignal from '../src/signal'
 
-test('start with red light ', t => {
+test('starts with red light', t => {
+  const signal = getSignal()
   const light = signal.getCurrent()
+
   t.is(light, 'red')
 })
 
 test('changes to yellow as second', t => {
+  const signal = getSignal()
+
   signal.change()
+
   const light = signal.getCurrent()
+
   t.is(light, 'yellow')
 })
 
 test('changes to green as third', t => {
+  const signal = getSignal()
+
   signal.change()
+  signal.change()
+
   const light = signal.getCurrent()
+
   t.is(light, 'green')
 })
 
 test('changes to yellow as fourth', t => {
+  const signal = getSignal()
+
   signal.change()
+  signal.change()
+  signal.change()
+
   const light = signal.getCurrent()
+
   t.is(light, 'yellow')
 })
 
-test('changes to read as fifth', t => {
+test('changes to red as fifth', t => {
+  const signal = getSignal()
+
   signal.change()
+  signal.change()
+  signal.change()
+  signal.change()
+
   const light = signal.getCurrent()
+
   t.is(light, 'red')
 })
